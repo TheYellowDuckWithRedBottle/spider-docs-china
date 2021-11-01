34b573cc54340a537a1df7d55e6ea23e4a0cb61a
Levi <1521681359@qq.com>
2021-11-01 22:43:41 +0800

    添加mongodb依赖

---
+++ src/main/java/spiderdocschina/dao/DosChinaDao.java
@@ -0,0 +1,10 @@
+package spiderdocschina.dao;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+public class DosChinaDao {
+    private MongoTemplate mongoTemplate;
+    public void saveDosChina(){
+
+    }
+}
